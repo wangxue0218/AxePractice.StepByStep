7c3cb36e0d7ea638d0eec15bf46f09ca4a7c362f
wangxue0218 <xuewang@thoughtworks.com>
2017-08-20 21:17:49 +0800

    Finish Http Content Negotiator

--- src/WebApi/practices/04_http_message_handling/src/SampleWebApi/MessageController.cs
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -14,9 +15,16 @@ namespace SampleWebApi
             // order to pass the test.
             // You can add new files if you want. But you cannot change any existed code.
 
-            throw new NotImplementedException();
+            return Request.CreateResponse(HttpStatusCode.OK, new ResultMessage {Message = "Hello"});
 
             #endregion
         }
+
+    }
+
+    [Serializable]
+    public class ResultMessage
+    {
+        public string Message { get; set; }
     }
 }
